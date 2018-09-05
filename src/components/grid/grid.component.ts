92cf0bce2b0036bcddcbb2239f9e02ef55c5a81a
chococrok <moi_viking@hotmail.com>
2018-09-05 15:52:57 +0200

    set the grid

--- src/components/grid/grid.component.ts
+++ src/components/grid/grid.component.ts
@@ -7,19 +7,22 @@ import { Component } from '@angular/core';
 })
 export class GridComponent {
   astres = [
-    { b: "b", },
-    { b: "c" },
-    { b: "dhfsoi,csjdfcswlhdfhclxw,f,ushfl,hdfl,dfkjclfjf djfldsjflkjcwflwjdsf hlkhflwfw", },
-    { b: "f", },
-    { b: "e", },
-    { b: "g", },
-    { b: "h", },
+    { b: "https://upload.wikimedia.org/wikipedia/commons/0/03/Vulpes_vulpes_laying_in_snow.jpg" },
+    { b: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Fox_study_6.jpg/399px-Fox_study_6.jpg" },
+    { b: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Fox_study_6.jpg/399px-Fox_study_6.jpg", },
+    { b: "https://peopledotcom.files.wordpress.com/2017/10/juniper-3.jpg?crop=81px,185px,625px,328px&resize=1200,630", },
+    { b: "https://fakeimg.pl/350x200/ff0000/000", },
+    { b: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Fox_study_6.jpg/399px-Fox_study_6.jpg", },
+    { b: "https://fakeimg.pl/350x200/ff0000/000", },
+    { b: "https://fakeimg.pl/350x200/ff0000/000", },
+    { b: "https://fakeimg.pl/350x200/ff0000/000", },
+    { b: "https://fakeimg.pl/350x200/ff0000/000", },
   ]
 
   doInfinite(infiniteScroll) {
     console.log('Begin async operation');
 
-    this.astres.push( {b: "HO"} );
+    this.astres.push( {b: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Fox_study_6.jpg/399px-Fox_study_6.jpg"} );
     infiniteScroll.complete();
   }
 }
